f07ff6238e0d3cfbc860c25122e0e4e818f417df
greenxiaye <greenxiaye@163.com>
2022-02-27 17:09:25 +0800

    刷新保存勾选

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -23,6 +23,14 @@ function App() {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
   }, [todos])
 
+  function toggleTodo(id) {
+    const newTodos = [...todos]
+    const todo = newTodos.find(todo => todo.id === id)
+    todo.complete = !todo.complete
+    setTodos(newTodos)
+  }
+
+  //添加todo
   function handleAddTodo(e) {
     const name = todoNameRef.current.value
     if (name === '') return
@@ -33,11 +41,11 @@ function App() {
   }
   return (
     <>
-      <TodoList todos={todos} />
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
       <input ref={todoNameRef} type="text" />
       <button onClick={handleAddTodo}>Add Todo</button>
       <button>Clear Complete</button>
-      <div>0 left to do </div>
+      <div>{todos.filter(todo => !todo.complete).length} left to do </div>
     </>
   )
 }
--- react-test/src/Todo.js
+++ react-test/src/Todo.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
-export default function Todo({ todo }) {
+export default function Todo({ todo, toggleTodo }) {
+    //刷新可以保存勾选
+    function handleTodoClick() {
+        toggleTodo(todo.id)
+    }
     return (
         <div>
             <label>
-                <input type="checkbox" checked={todo.complete} />
+                <input type="checkbox" checked={todo.complete} onChange={handleTodoClick} />
                 {todo.name}
             </label>
         </div>
