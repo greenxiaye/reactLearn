91ea2085f867e1b0764cfd67b9c55ffe0bad6b4d
greenxiaye <greenxiaye@163.com>
2022-02-28 00:56:29 +0800

    重写旧值

--- react-hooks/useStateLearn/src/App.js
+++ react-hooks/useStateLearn/src/App.js
@@ -1,24 +1,26 @@
 import React, { useState } from 'react';
 
-function countInitial() {
-  console.log('run function')
-  return 4
-}
-
 function App() {
-  const [count, setCount] = useState(() => countInitial())
+  const [state, setState] = useState({ count: 4, theme: 'blue' })
+  const count = state.count
+  const theme = state.theme
 
   function decrementCount() {
-    setCount(prevCount => prevCount - 1)
+    setState(prevCount => {
+      return { count: prevCount.count - 1 }
+    })
   }
 
   function incrementCount() {
-    setCount(prevCount => prevCount + 1)
+    setState(prevCount => {
+      return { count: prevCount.count + 1 }
+    })
   }
   return (
     <>
       <button onClick={decrementCount}>-</button>
       <span>{count}</span>
+      <span>{theme}</span>
       <button onClick={incrementCount}>+</button>
     </>
   )
