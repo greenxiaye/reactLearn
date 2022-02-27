efe53e0ee9c19c0177ba79ec2c6f2b2d9609a6be
greenxiaye <greenxiaye@163.com>
2022-02-27 16:58:00 +0800

    使用useEffect

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -1,11 +1,27 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import TodoList from './TodoList'
 import { v4 as uuidv4 } from 'uuid';
 // const { v4: uuidv4 } = require('uuid');
 
+const LOCAL_STORAGE_KEY = "todoApp.todos"
+
 function App() {
   const [todos, setTodos] = useState([])
   const todoNameRef = useRef()
+
+  // 刷新的时候，取出来
+  useEffect(() => {
+    //JSON.parse恢复成array
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    console.log(storedTodos)
+    if (storedTodos)
+      setTodos(storedTodos)
+  }, [])
+
+  // 存起来
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
 
   function handleAddTodo(e) {
     const name = todoNameRef.current.value
