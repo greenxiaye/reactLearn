05b796c0700477f77913e6f76206af1e0ce56460
greenxiaye <greenxiaye@163.com>
2022-02-28 00:04:25 +0800

    count初始值一样

--- react-hooks/useStateLearn/src/App.js
+++ react-hooks/useStateLearn/src/App.js
@@ -4,7 +4,8 @@ function App() {
   const [count, setCount] = useState(4)
 
   function decrementCount() {
-    setCount(prevCount => prevCount - 1)
+    setCount(count - 1)
+    setCount(count - 1)
   }
   return (
     <>
