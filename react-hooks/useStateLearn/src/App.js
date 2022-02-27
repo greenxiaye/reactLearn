d19785bda9d57332b0cef28982ae672d49b776b7
greenxiaye <greenxiaye@163.com>
2022-02-28 00:41:34 +0800

    只console一遍

--- react-hooks/useStateLearn/src/App.js
+++ react-hooks/useStateLearn/src/App.js
@@ -1,17 +1,23 @@
 import React, { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(4)
+  const [count, setCount] = useState(() => {
+    console.log('run function')
+    return 4
+  })
 
   function decrementCount() {
     setCount(prevCount => prevCount - 1)
-    setCount(prevCount => prevCount - 1)
+  }
+
+  function incrementCount() {
+    setCount(prevCount => prevCount + 1)
   }
   return (
     <>
       <button onClick={decrementCount}>-</button>
       <span>{count}</span>
-      <button>+</button>
+      <button onClick={incrementCount}>+</button>
     </>
   )
 }
