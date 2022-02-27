2f2e2212ae52bd22dfd31abddb6bd3f584f94cbe
greenxiaye <greenxiaye@163.com>
2022-02-27 17:12:53 +0800

    增加清空已做的功能点

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -39,12 +39,18 @@ function App() {
     })
     todoNameRef.current.value = null
   }
+
+  // 删除已做的
+  function handleClearTodos() {
+    const newTodos = todos.filter(todo => !todo.complete)
+    setTodos(newTodos)
+  }
   return (
     <>
       <TodoList todos={todos} toggleTodo={toggleTodo} />
       <input ref={todoNameRef} type="text" />
       <button onClick={handleAddTodo}>Add Todo</button>
-      <button>Clear Complete</button>
+      <button onClick={handleClearTodos}>Clear Complete</button>
       <div>{todos.filter(todo => !todo.complete).length} left to do </div>
     </>
   )
