6024dd2d07a03dd8ff9c5573b5cb059b928ed380
greenxiaye <greenxiaye@163.com>
2022-02-28 00:45:30 +0800

    渲染console多次

--- react-hooks/useStateLearn/src/App.js
+++ react-hooks/useStateLearn/src/App.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 
+function countInitial() {
+  console.log('run function')
+  return 4
+}
+
 function App() {
-  const [count, setCount] = useState(() => {
-    console.log('run function')
-    return 4
-  })
+  const [count, setCount] = useState(countInitial())
 
   function decrementCount() {
     setCount(prevCount => prevCount - 1)
