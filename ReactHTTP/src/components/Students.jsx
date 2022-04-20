940b36b18cafea4df0bad5ea0ea4284774de82de
greenxiaye <greenxiaye@163.com>
2022-04-21 00:55:59 +0800

    HTTP工具组件使用

--- ReactHTTP/src/components/Students.jsx
+++ ReactHTTP/src/components/Students.jsx
@@ -1,3 +1,4 @@
+import HTTP from '../utils/HTTP';
 class Students extends React.Component {
     render() {
         return (
@@ -11,7 +12,32 @@ class Students extends React.Component {
                         </tr>
                     </thead>
                     <tbody>
-                        
+                        <HTTP.Get
+                            url="http://localhost:8080/getStudents"
+                            loading={
+                                <tr>
+                                    <td colspan="3">loading</td>
+                                </tr>
+                            }
+                            error={
+                                <tr>
+                                    <td colspan="3">Error</td>
+                                </tr>
+                            }
+                        >
+                            {
+                                // 回调
+                                (data)=>{
+                                    return data.map(item=>{
+                                        <tr key={item.id}>
+                                            <td>{item.id}</td>
+                                            <td>{item.name}</td>
+                                            <td>{item.grade}</td>
+                                        </tr>
+                                    })
+                                }
+                            }
+                        </HTTP.Get>
                     </tbody>
                 </table>
             </div>
